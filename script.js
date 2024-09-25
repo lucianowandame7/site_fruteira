7559e77f23f668dcc98dffe84d3cc591cba89a95
Luciano Wandame <luwandame@gmail.com>
2024-09-25 16:13:23 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var carrinho = document.querySelectorAll('.carrinho');
+
+carrinho[0].addEventListener('click',()=>{
+    Let menu = document.getElementById('menu');
+    if(menu.classList.contains('hide')){{}}
+});
